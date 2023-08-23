8fccbbca1c753fad3cfb8d81f5ecefd2f998a9d9
Hyunjun Angelo Jeon <hajccts417@gmail.com>
2023-08-23 15:24:01 -0500

    Add prompt for password length input & fix connection to stylesheet and script on html file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,28 @@
 // Assignment code here
+function generatePassword() {
+  // Number
+  var passwordLength;
 
+  do {
+    passwordLength = passwordCriteriaLength();
+  }
+  while (passwordLength === undefined);
+  
+}
+
+function passwordCriteriaLength() {
+  var inputLength = Number(prompt("Please input your desired character length for your password (Between 8 and 128)"));
+
+  if (inputLength >= 8 && inputLength <= 128) {
+    return inputLength;
+  }
+  else if (inputLength <= 8 || inputLength >= 128) {
+    alert("Please provide a valid range.");
+  }
+  else {
+    alert("Please provide a valid input");
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
