61a24a48e269e896d7c74ca05265ed18df4bc348
Hyunjun Angelo Jeon <hajccts417@gmail.com>
2023-08-24 14:25:07 -0500

    Testing logs removed, added more descriptive comments, clarified various names

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,15 @@
-var lowercaseString = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericString = "1234567890";
-var specialString = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-// console.log(specialString.length);
-// console.log(specialString);
-// console.log(specialString.substring(0, 18));
+// Strings for each character type that a generated password can have.
+var LOWERCASE_STRING = "abcdefghijklmnopqrstuvwxyz";
+var UPPERCASE_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var NUMERIC_STRING = "1234567890";
+var SPECIAL_STRING = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-// Assignment code here
+// Main function
 function generatePassword() {
-  // Number
-  var passwordLength;
-  // Booleans
-  let passwordChar = {
+  // Number, determines generated password length
+  var criteriaLength;
+  // Booleans, determines contents of generated password
+  let criteriaCharType = {
     lowercase: false,
     uppercase: false,
     numeric: false,
@@ -29,16 +27,16 @@ function generatePassword() {
     totalStringSample: function() {
       var totalSample = "";
       if (this.lowercase) {
-        totalSample += lowercaseString;
+        totalSample += LOWERCASE_STRING;
       }
       if (this.uppercase) {
-        totalSample += uppercaseString;
+        totalSample += UPPERCASE_STRING;
       }
       if (this.numeric) {
-        totalSample += numericString;
+        totalSample += NUMERIC_STRING;
       }
       if (this.special) {
-        totalSample += specialString;
+        totalSample += SPECIAL_STRING;
       }
       return totalSample;
     },
@@ -46,16 +44,16 @@ function generatePassword() {
     returnMinimumRequirement: function() {
       var minReq = "";
       if (this.lowercase) {
-        minReq += this.randomFromString(lowercaseString);
+        minReq += this.randomFromString(LOWERCASE_STRING);
       }
       if (this.uppercase) {
-        minReq += this.randomFromString(uppercaseString)
+        minReq += this.randomFromString(UPPERCASE_STRING)
       }
       if (this.numeric) {
-        minReq += this.randomFromString(numericString);
+        minReq += this.randomFromString(NUMERIC_STRING);
       }
       if (this.special) {
-        minReq += this.randomFromString(specialString);
+        minReq += this.randomFromString(SPECIAL_STRING);
       }
       return minReq;
     },
@@ -67,38 +65,37 @@ function generatePassword() {
 
   // Ask for valid character length input, repeat until valid range is given.
   do {
-    passwordLength = passwordCriteriaLength();
+    criteriaLength = promptCriteriaLength();
   }
-  while (passwordLength === undefined);
-  // console.log(passwordLength);
+  while (criteriaLength === undefined);
 
   // Ask for valid character type input, repeat until at least one type is chosen.
   do {
-    var criteria = passwordCriteriaType();
-    passwordChar.lowercase = criteria[0];
-    passwordChar.uppercase = criteria[1];
-    passwordChar.numeric = criteria[2];
-    passwordChar.special = criteria[3];
-    // console.log(passwordChar);
-    // console.log(criteria);
+    var criteria = promptCriteriaType();
+    criteriaCharType.lowercase = criteria[0];
+    criteriaCharType.uppercase = criteria[1];
+    criteriaCharType.numeric = criteria[2];
+    criteriaCharType.special = criteria[3];
   }
-  while (passwordChar.isAllFalse());
+  while (criteriaCharType.isAllFalse());
 
-  // Generate Password, at least the characters that are contained in the password
-  var characterSample = passwordChar.totalStringSample();
+  // Generate Password
+  // First input one character from each type of character the password will have.
+  var characterSample = criteriaCharType.totalStringSample();
   var genPass = ""; // Generated Password
-  for (var i=0; i<passwordLength; i++) {
+  for (var i=0; i<criteriaLength; i++) {
     if (i === 0) {
-      genPass += passwordChar.returnMinimumRequirement();
+      genPass += criteriaCharType.returnMinimumRequirement();
       i += genPass.length;
     }
-    genPass += passwordChar.randomFromString(characterSample);
+    genPass += criteriaCharType.randomFromString(characterSample);
   }
 
-  // Shuffle the password, so up to first 4 characters of the password aren't ordered by lowercase, uppercase, number, than special character
-  // Fisher-Yates shuffle
+  // Shuffle the password.
+  // Ensure so up to first 4 characters of the password aren't ordered by lowercase, uppercase, number, then special character.
+  // Fisher-Yates shuffle.
   var genPass = genPass.split("");
-  for (var i = passwordLength - 1; i > 0; i--) {
+  for (var i = criteriaLength - 1; i > 0; i--) {
     var replaceIndex = Math.floor(Math.random() * (i + 1));
     var tempChar = genPass[i];
     genPass[i] = genPass[replaceIndex];
@@ -108,7 +105,10 @@ function generatePassword() {
   return genPass.join("");
 }
 
-function passwordCriteriaLength() {
+// Prompts user to input a number between a certain range.
+// On valid input, returns input.
+// On invalid input, alerts user then returns null.
+function promptCriteriaLength() {
   var inputLength = Number(prompt("Please input your desired character length for your password (Between 8 and 128)"));
 
   if (inputLength >= 8 && inputLength <= 128) {
@@ -122,7 +122,11 @@ function passwordCriteriaLength() {
   }
 }
 
-function passwordCriteriaType() {
+// Series of prompts to confirm or deny including certain character types.
+// Returns an array of a specific size, containing all booleans.
+// If even a single index is true, returns array.
+// If all contents of array are false, alert user then returns null.
+function promptCriteriaType() {
   var criteria = [false, false, false, false];
   var criteriaCheck = function(message) {
     var inputBool = prompt(message + "(Y/N)");
@@ -134,7 +138,7 @@ function passwordCriteriaType() {
     }
     else {
       alert("Please input either Y or N in either lower or uppercase");
-      return passwordCriteriaType();
+      return promptCriteriaType();
     }
   }
 
